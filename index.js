2ccd78215382f2521aba752bb92c4beba2053bc2
ben <bentan1020@gmail.com>
2023-11-16 10:39:34 -0500

    Initial boilerplate for setting up express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// define middlewares
+app.use("'/api/alexa", )
+
+port = 3000;
+app.listen(port, () => {
+  console.log(`Port is currently running on ${port}`);
+});
